655505b1c58c8587ea8b5b3e36c8333a99041c3a
Sabona <sebonawaktole@gmail.com>
2026-03-31 17:52:08 +0300

    complete redesign of the landing page

--- frontend/src/app/certificate/verify/[uniqueId]/page.tsx
+++ frontend/src/app/certificate/verify/[uniqueId]/page.tsx
@@ -1,6 +1,10 @@
 import VerifyClient from './VerifyClient';
 import React from 'react';
 
+export function generateStaticParams() {
+    return [{ uniqueId: 'dummy' }];
+}
+
 export default function VerifyCertificatePage({ params }: { params: Promise<{ uniqueId: string }> }) {
     const resolvedParams = React.use(params);
     return <VerifyClient uniqueId={resolvedParams.uniqueId} />;
